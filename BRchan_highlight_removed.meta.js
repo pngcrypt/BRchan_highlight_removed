3f594f935be1dc8b2191b3ca12b938231a6f39b8
pngcrypt <png.crypt@yandex.ru>
2016-12-11 19:14:05 +0300

    ver up

--- BRchan_highlight_removed.meta.js
+++ BRchan_highlight_removed.meta.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name         BRchan highlight removed
 // @namespace    https://www.brchan.org/
-// @version      1.0.2
+// @version      1.0.3
 // @author       yugo-salem, pngcrypt
 // @updateURL    https://raw.github.com/pngcrypt/BRchan_highlight_removed/master/BRchan_highlight_removed.meta.js
 // @include      http*://www.brchan.org/*
