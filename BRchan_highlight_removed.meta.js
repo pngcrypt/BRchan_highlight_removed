9121201c2e869d961250ed0e756176d0b06d4428
pngcrypt <pngcrypt@users.noreply.github.com>
2017-01-22 23:06:59 +0300

    ver up

--- BRchan_highlight_removed.meta.js
+++ BRchan_highlight_removed.meta.js
@@ -1,11 +1,12 @@
 ﻿// ==UserScript==
 // @name         BRchan highlight removed
 // @namespace    https://www.brchan.org/
-// @version      1.0.4
+// @version      1.0.5
 // @author       yugo-salem, pngcrypt
 // @updateURL    https://raw.github.com/pngcrypt/BRchan_highlight_removed/master/BRchan_highlight_removed.meta.js
 // @include      http*://www.brchan.org/*
 // @include      http*://brchan.org/*
+// @include      http://brchanansdnhvvnm.onion/*
 // @grant        none
 // @run-at       document-idle
 // ==/UserScript==
