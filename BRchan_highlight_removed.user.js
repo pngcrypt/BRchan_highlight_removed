a0eb815569aa59361a5499fab77cbda8a99a5360
pngcrypt <pngcrypt@users.noreply.github.com>
2017-01-22 23:04:13 +0300

    jq 3.1 fix

--- BRchan_highlight_removed.user.js
+++ BRchan_highlight_removed.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BRchan highlight removed
 // @namespace    https://www.brchan.org/
-// @version      1.0.4
+// @version      1.0.5
 // @author       yugo-salem, pngcrypt
 // @updateURL    https://raw.github.com/pngcrypt/BRchan_highlight_removed/master/BRchan_highlight_removed.meta.js
 // @include      http*://www.brchan.org/*
@@ -38,7 +38,7 @@
 			processData: false,
 			dataType: 'text'
 		})
-		.success(function (data) {
+		.done(function (data) {
 			var latestPosts = getPosts($(document)); // get posts from page
 			if(!latestPosts) 
 				return; // wrong year...
